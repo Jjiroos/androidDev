d96cee0b4cc36996ca4ea99221b4b96ccaede6d5
Jïros <cloclo2412@gmail.com>
2022-03-03 16:04:00 +0100

    ajout de la page d'accueil et des toast

--- app/src/main/java/fr/isen/cremades/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/cremades/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,32 @@ package fr.isen.cremades.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // get reference to button
+        val choixEntree = findViewById<TextView>(R.id.choixEntree)
+        // set on-click listener
+        choixEntree.setOnClickListener {
+            Toast.makeText(this, "Soon => liste des entrées disponible", Toast.LENGTH_SHORT).show()
+        }
+
+        val choixPlat = findViewById<TextView>(R.id.choixPlat)
+        // set on-click listener
+        choixPlat.setOnClickListener {
+            Toast.makeText(this, "Soon => liste des plats disponible", Toast.LENGTH_SHORT).show()
+        }
+
+        val choixDessert = findViewById<TextView>(R.id.choixDessert)
+        // set on-click listener
+        choixDessert.setOnClickListener {
+            Toast.makeText(this, "Soon => liste des desserts disponible", Toast.LENGTH_SHORT).show()
+        }
     }
 }
