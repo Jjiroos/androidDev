47ad2c418ac0196bc3c132a3820cfacf096f6722
Jïros <cloclo2412@gmail.com>
2022-03-03 16:28:33 +0100

    ajout de la redirection + les logs

--- app/src/main/java/fr/isen/cremades/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/cremades/androiderestaurant/HomeActivity.kt
@@ -1,33 +1,58 @@
 package fr.isen.cremades.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
+    companion object{
+        const val ORDER_KEY="order";
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        // get reference to button
+        // init the button references
         val choixEntree = findViewById<TextView>(R.id.choixEntree)
-        // set on-click listener
+        val choixPlat = findViewById<TextView>(R.id.choixPlat)
+        val choixDessert = findViewById<TextView>(R.id.choixDessert)
+
+        // set on-click listener for
         choixEntree.setOnClickListener {
-            Toast.makeText(this, "Soon => liste des entrées disponible", Toast.LENGTH_SHORT).show()
+            displayOrder(getString(R.string.choixEntree))
         }
 
-        val choixPlat = findViewById<TextView>(R.id.choixPlat)
+
         // set on-click listener
         choixPlat.setOnClickListener {
-            Toast.makeText(this, "Soon => liste des plats disponible", Toast.LENGTH_SHORT).show()
+            displayOrder(getString(R.string.choixPlat))
         }
 
-        val choixDessert = findViewById<TextView>(R.id.choixDessert)
+
         // set on-click listener
         choixDessert.setOnClickListener {
-            Toast.makeText(this, "Soon => liste des desserts disponible", Toast.LENGTH_SHORT).show()
+            displayOrder(getString(R.string.choixDessert))
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("HomeActivity", "l'activité est arrétée")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d("HomeActivity", "l'activité est détruite")
+    }
+
+    private fun displayOrder(message: CharSequence) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, OrderActivity::class.java)
+        startActivity(intent)
     }
 }
